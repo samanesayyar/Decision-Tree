d39764bf685262c9fccfa97b554f90dc30dd24ee
AI_Sayyar <samanesayyar7210@gmail.com>
2024-01-24 08:51:18 +0330

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,6 +1,5 @@
 
 class Preprocessing:
-
     def encoding(self, dataset, features):
         for i in range(len(features)):
             dataset.iloc[:, i] = [features[i][item] for item in dataset.iloc[:, i].values]
